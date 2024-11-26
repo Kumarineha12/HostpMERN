ad00ef26ea74065e0b53c5eebd5807b8692a282b
Kumarineha12 <99068535+Kumarineha12@users.noreply.github.com>
2024-11-26 10:55:54 +0530

    Update App.jsx

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          ~${server}/api/v1/user/admin/me`,
+          `${server}/api/v1/user/admin/me`,
           {
             withCredentials: true,
           }
