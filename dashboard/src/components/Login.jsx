1ac9e85c860680e72b2c41e7690459c2c29957be
Kumarineha12 <99068535+Kumarineha12@users.noreply.github.com>
2024-11-26 10:54:17 +0530

    Update Login.jsx

--- dashboard/src/components/Login.jsx
+++ dashboard/src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import { Context,server } from "../main";
+import { Context, server } from "../main";
 import axios from "axios";
 
 const Login = () => {
